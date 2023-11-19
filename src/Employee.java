5c649f37a2cfd12e459252c8a238f3c36b19623a
Илон <vlasovvdm@gmail.com>
2023-11-19 09:48:25 +0400

    Базовая сложность

---
+++ src/Employee.java
@@ -0,0 +1,69 @@
+import java.util.Objects;
+
+public class Employee {
+   private String name;
+    private int department;
+    private double salary;
+    private final int id;
+    private static int COUNTER=1;
+
+    public Employee(String name, int department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.id=COUNTER;
+        COUNTER++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && Double.compare(salary, employee.salary) == 0 && id == employee.id && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, department, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employess = new Employee[10];
+        Random random = new Random();
+        for (int i = 0; i < employess.length; i++) {
+            double salary = 50_000 + 50_000 * random.nextDouble();
+            employess[i] = new Employee("name" + i, random.nextInt(1, 6), salary);
+        }
+
+        employAlL(employess);
+        System.out.println();
+        System.out.println(getSum(employess));
+        System.out.println(getMinSalary(employess));
+        System.out.println(getMaxSalary(employess));
+        System.out.println(getAverageSalary(employess));
+        System.out.println();
+        employAlLs(employess);
+    }
+
+    private static void employAlL(Employee[] employess) {
+        for (Employee employee : employess) {
+            System.out.println(employee);
+        }
+    }
+
+    public static double getSum(Employee[] employess) {
+        double sum = 0;
+        for (Employee employee : employess) {
+            sum = sum + employee.getSalary();
+        }
+        return sum;
+    }
+
+    public static Employee getMinSalary(Employee[] employess) {
+        Employee minEmployee = null;
+        for (Employee employee : employess) {
+            if (minEmployee == null || minEmployee.getSalary() > employee.getSalary()) {
+                minEmployee = employee;
+            }
+        }
+        return minEmployee;
+    }
+
+    public static Employee getMaxSalary(Employee[] employess) {
+        Employee maxEmployee = null;
+        for (Employee employee : employess) {
+            if (maxEmployee == null || maxEmployee.getSalary() < employee.getSalary()) {
+                maxEmployee = employee;
+            }
+        }
+        return maxEmployee;
+    }
+
+    public static double  getAverageSalary(Employee[] employess) {
+        return getSum(employess) / employess.length;
+        }
+
+
+
+    private static void employAlLs(Employee[] employess) {
+        for (Employee employee : employess) {
+            System.out.println(employee.getName());
+        }
     }
 }
+
+
+
+
+
